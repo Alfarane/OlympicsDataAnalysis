d56ecc54511d53b08ffc0ee5985c08030d236d0d
Alfarane <alpharane22@gmail.com>
2024-08-05 22:40:22 +0530

    second commit

--- app.py
+++ app.py
@@ -2,7 +2,10 @@ import streamlit as st
 import pandas as pd
 import preprocessor,helper
 
-df = preprocessor.preprocess()
+df = pd.read_csv('athlete_events.csv')
+region_df = pd.read_csv('noc_regions.csv')
+
+df = preprocessor.preprocess(df,region_df)
 
 user_menu = st.sidebar.radio(
     'Select an Option',
@@ -14,4 +17,5 @@ st.dataframe(df)
 if user_menu == 'Medal Tally':
     medal_tally = helper.medal_tally(df)
     st.dataframe(medal_tally)
+
 
